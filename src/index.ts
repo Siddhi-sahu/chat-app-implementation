8d81817fc71130da745d0ec8daab01052fdd5958
Siddhi Sahu <himanisahu739@gmail.com>
2025-01-30 13:40:05 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
-import { createServer } from "http"
+import { createServer } from "http";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const app = express();
 
 const server = createServer(app);
 
+const _dirname = dirname(fileURLToPath(import.meta.url))
 app.get("/", (req, res) => {
-    res.send('<h1>Hello world</h1>')
+    res.sendFile(join(_dirname, '../src/index.html'))
+
 });
 
 app.listen(3000, () => {
