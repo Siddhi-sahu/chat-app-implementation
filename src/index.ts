cb7bbce6437beaab9401cb19c09fe214e4ba788f
Siddhi Sahu <himanisahu739@gmail.com>
2025-01-30 15:44:14 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import express from "express";
 import { createServer } from "http";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import { Server } from "socket.io";
 
 const app = express();
 
+//manually creating a server instead of  Express’s built-in one (This is useful when you need to extend the server’s functionality, like:
+//*Using WebSockets (socket.io).
+//*Handling both HTTP and WebSocket connections on the same server.
+//)
 const server = createServer(app);
+
+//This binds Socket.io to the same HTTP server.
+const io = new Server(server);
+
 
 const _dirname = dirname(fileURLToPath(import.meta.url))
 app.get("/", (req, res) => {
@@ -13,6 +22,15 @@ app.get("/", (req, res) => {
 
 });
 
-app.listen(3000, () => {
+// I listen on the connection event for incoming sockets 
+io.on('connection', (socket) => {
+    console.log("a user connected");
+
+    socket.on('disconnect', () => {
+        console.log("user disconnected")
+    })
+})
+
+server.listen(3000, () => {
     console.log("server running at http://localhost:3000")
 })
